5ee67eee1a40d5336d1f48a07b6fed9c30bd3d22
Лариса Савицкая <m.turguts@hotmail.com>
2023-11-30 12:05:05 +0100

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
     entry: {
-       index : './src/index.js'
+       index : './src/index.js',
     },
     output: {
         filename: '[name].js',
